36378ef0a2ce3071b8c21dc178515778652109fd
Lyon <lyon@delorum.com>
2011-10-20 10:29:51 -0600

    here we go again on our own

--- http-git2.js
+++ http-git2.js
@@ -13,7 +13,14 @@ require('https').createServer(options, require('stack')(
 		var pathname = url.parse(req.url).pathname;
 		console.log ("path : " + pathname)
 	},
-	cgi('git http-backend', {}))
+	cgi('git http-backend', {
+		PATH_INFO: "/mnt/git" + pathname,
+		REMOTE_USER: "lyon",
+		REMOTE_ADDR: "127.0.0.1",
+		CONTENT_TYPE: req.headers.accept,
+		QUERY_STRING: url.parse(req.url).query;
+		REQUEST_METHOD: req.method
+	}))
 
 
 ).listen(443, "50.97.133.246");
